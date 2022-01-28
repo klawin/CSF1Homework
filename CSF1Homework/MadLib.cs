39075cbcb8e3e2dea8295c9a5a2e846e21fe8bdf
Kayla Nguyen <kaylannguyen@outlook.com>
2022-01-28 16:03:39 -0600

    Warm up: Mad Lib

---
+++ CSF1Homework/MadLib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class MadLib
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Warm Up: Mad Lib";
+            Console.WriteLine("Warm Up: Mad Lib");
+
+            string[] input = new string[3];
+
+            Console.Write("Enter an adjective: ");
+            input[0] = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            input[1] = Console.ReadLine();
+
+            Console.Write("Enter a noun: ");
+            input[2] = Console.ReadLine();
+
+            Console.WriteLine($"The {input[2]} will {input[1]} until it is {input[0]}!");
+
+
+
+        }//end Main()
+    }//end class
+}//end namespace
