a9c42a582a46057d47828926f23ad756031fbf0b
Kayla Nguyen <kaylannguyen@outlook.com>
2022-01-28 15:50:50 -0600

    Warm up Fruit Loops

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Warm Up: Fruit Loop 1";
+            Console.WriteLine("Warm Up: Fruit Loop 1");
+
+            string[] fruits = { "apple", "orange", "pear", "watermelon", "banana", "strawberry" };
+            
+            for (int x = 0; x < 6; x++)
+            {
+                Console.WriteLine(fruits[x]);
+            }
+
+        }//end Main()
+    }//end class
+}//end namescape
---
+++ CSF1Homework/FruitLoop2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop2
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Warm Up: Fruit Loop 2";
+            Console.WriteLine("Warm Up: Fruit Loop 2");
+
+            string[] fruits = { "apple", "orange", "pear", "watermelon", "banana", "strawberry" };
+            string y = ".";
+            for (int x = 0; x < 6; x++, y+=".")
+            {
+                Console.WriteLine(fruits[x]);
+                Console.WriteLine(y);
+            }
+
+        }//end Main()
+    }//end class
+}//end namespace
