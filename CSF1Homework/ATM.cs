1a1d9aee6664c3cbcb6bea75e39cf26eb2aacad5
Kayla Nguyen <kaylannguyen@outlook.com>
2022-01-29 22:03:12 -0600

    Additional Optional Features

--- CSF1Homework/ATM.cs
+++ CSF1Homework/ATM.cs
@@ -13,6 +13,7 @@ namespace CSF1Homework
             Console.Title = "ATM";
             Console.WriteLine("Welcome!");
 
+            decimal acctBalance = 0;
 
             bool repeat = true;
             int attempts = 1;
@@ -39,7 +40,8 @@ namespace CSF1Homework
                             {
                                 Console.WriteLine("1) Deposit\n" +
                                     "2) Withdrawal\n" +
-                                    "3) Exit\n");
+                                    "3) Balance\n" +
+                                    "4) Exit\n");
                                 ConsoleKey userChoice = Console.ReadKey(true).Key;
                                 switch (userChoice)
                                 {
@@ -51,19 +53,20 @@ namespace CSF1Homework
                                         Console.Write("How much would you like to deposit? ");
                                         decimal depositAmt = Convert.ToDecimal(Console.ReadLine());
                                         Console.WriteLine("\n{0:c} will be desposited into account number {1}.", depositAmt, userAcctNbr);
+                                        Console.WriteLine("\nAccount balance: {0:c}", acctBalance += depositAmt);
                                         Console.WriteLine("\nWould you like to make another transaction? Y/N");
                                         ConsoleKey anotherTrans = Console.ReadKey(true).Key;
                                         if (anotherTrans == ConsoleKey.Y)
                                         {
                                             Console.Clear();
                                             repeat = true;  
-                                        }
+                                        }//end if
                                         else
                                         {
                                             Console.Clear();
                                             Console.WriteLine("Thank you! Have a great day.");
                                             repeat = false;
-                                        }
+                                        }//end else
                                        
                                         break;
 
@@ -75,23 +78,42 @@ namespace CSF1Homework
                                         Console.Write("How much would you like to withdrawal? ");
                                         decimal withdrawalAmt = Convert.ToDecimal(Console.ReadLine());
                                         Console.WriteLine("\n{0:c} will be withdrawn from account number {1}.", withdrawalAmt, userAcctNbr);
+                                        Console.WriteLine("\nAccount balance: {0:c}", acctBalance -= withdrawalAmt);
                                         Console.WriteLine("\nWould you like to make another transaction? Y/N");
                                         anotherTrans = Console.ReadKey(true).Key;
                                         if (anotherTrans == ConsoleKey.Y)
                                         {
                                             Console.Clear();
                                             repeat = true;
-                                        }
+                                        }//end if
                                         else
                                         {
                                             Console.Clear();
                                             Console.WriteLine("Thank you! Have a great day.");
                                             repeat = false;
-                                        }
+                                        }//end else
                                         break;
 
                                     case ConsoleKey.D3:
                                     case ConsoleKey.NumPad3:
+                                    case ConsoleKey.B:
+                                        Console.WriteLine("Account balance: {0:c}", acctBalance);
+                                        Console.WriteLine("\nWould you like to make another transaction? Y/N");
+                                        anotherTrans = Console.ReadKey(true).Key;
+                                        if (anotherTrans == ConsoleKey.Y)
+                                        {
+                                            Console.Clear();
+                                            repeat = true;
+                                        }//end if
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine("Thank you! Have a great day.");
+                                            repeat = false;
+                                        }//end else
+                                        break;
+                                    case ConsoleKey.D4:
+                                    case ConsoleKey.NumPad4:
                                     case ConsoleKey.X:
                                     case ConsoleKey.E:
                                         Console.WriteLine("Thank you. Have a great day!");
@@ -101,37 +123,37 @@ namespace CSF1Homework
                                         Console.WriteLine("I'm sorry. I did not understand. Please try again.");                        
 
                                         break;
-                                }
-                            } while (repeat);
+                                }//end switch
+                            } while (repeat);//end do while
 
-                        }
+                        }//end if
 
                         else if (attempts == 3)
                         {
                             Console.WriteLine("Access denied.");
                             repeat = true;
-                        }
+                        }//end else if
 
                         else
                         {
                             Console.WriteLine("Access denied. Try again");
                             repeat = true;
-                        }
+                        }//end else
 
                         attempts++;
 
-                    } while (attempts <=3 && repeat);
-                }
+                    } while (attempts <=3 && repeat);//end do while
+                }//end if
                 else if (attempts == 3)
                 {
                     Console.WriteLine("Account not found.");
-                }
+                }//end else if
                 else
                 {
                     Console.WriteLine("Account not found. Try again.");
-                }
+                }//end else
                 attempts++;
-            } while (repeat && attempts <= 3);
+            } while (repeat && attempts <= 3);//end do while
         }//end main()
     }//end class
 }//end namespace
